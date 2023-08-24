d4548837ce0e95ed3c0342202bcb44d73e573caa
David Liao <35374714+davidycliao@users.noreply.github.com>
2023-08-24 10:40:51 +0100

    UPDATE

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,42 @@
+#' #' @export .onAttach
+#' .onAttach <- function(...) {
+#'   message("## flaiR: An R Wrapper for Accessing Flair NLP Tagging Features   ##")
+#'   # Report Python version
+#'   python_version <- reticulate::py_config()$version
+#'   message("## Using Python:    ", python_version, "                                           ##")
+#'
+#'   # Check if flair is installed
+#'   if (check_flair_installed()) {
+#'     flair_version <- get_flair_version()
+#'     message("## Using Flair : ", flair_version, "                                           ##")
+#'   } else {
+#'     message("## Using Flair : ", "not installed in the current Python environment.    ##")
+#'   }
+#' }
+
 #' @export .onAttach
 .onAttach <- function(...) {
-  message("## flaiR: An R Wrapper for Accessing Flair NLP Tagging Features   ##")
+  header_footer <- "## ============================================================== ##"
+  message(header_footer)
+
+  message(sprintf("## flaiR: An R Wrapper for Accessing Flair NLP Tagging Features   ##"))
+
   # Report Python version
   python_version <- reticulate::py_config()$version
-  message("## Using Python:    ", python_version, "                                           ##")
+  message(sprintf("## Using Python: %-48s ##", python_version))
 
   # Check if flair is installed
   if (check_flair_installed()) {
     flair_version <- get_flair_version()
-    message("## Using Flair : ", flair_version, "                                           ##")
+    message(sprintf("## Using Flair:  %-48s ##", flair_version))
+
   } else {
-    message("## Using Flair : ", "not installed in the current Python environment.    ##")
+    message(sprintf("## Using Flair:  %-47s ##", "not installed in the current Python environment."))
   }
+
+  message(header_footer)
 }
+
 
 #' @export check_flair_installed
 check_flair_installed <- function(...) {
