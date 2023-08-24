519d793e6c3ccd38727d5d45f74dc7f641d2d568
David Liao <35374714+davidycliao@users.noreply.github.com>
2023-08-24 21:58:20 +0100

    Update test_get_entities.R

--- tests/testthat/test_get_entities.R
+++ tests/testthat/test_get_entities.R
@@ -1,3 +1,4 @@
+
 test_that("get_pos throws an error for unsupported languages", {
   expect_error(get_pos(
     texts = c("UCD is one of the best university in Ireland. ",
@@ -8,5 +9,33 @@ test_that("get_pos throws an error for unsupported languages", {
               "TCD in less better than Oxford"),
     doc_ids= c("doc1", "doc2", "doc3", "doc4", "doc5", "doc6"),
     language  = "chinese"),
-    regexp = "Unsupported language.")
+    regexp = "flair is not installed in the current Python environment.")
 })
+
+
+#
+#
+# # Define a test
+# library(testthat)
+# test_that("get_pos throws an error for unsupported languages and Flair not installed", {
+#   # Test unsupported language error
+#   expect_error(
+#     get_pos(
+#       texts = c("UCD is one of the best university in Ireland."),
+#       doc_ids = c("doc1"),
+#       language = "chinese"
+#     ),
+#     regexp = "Unsupported language."
+#   )
+#
+#   # Test Flair not installed error
+#   # Assume that the error message for Flair not installed is "flair is not installed in the current Python environment."
+#   expect_error(
+#     get_pos(
+#       texts = c("UCD is one of the best university in Ireland."),
+#       doc_ids = c("doc1"),
+#       language = "chinese"
+#     ),
+#     regexp = "flair is not installed in the current Python environment."
+#   )
+# })
