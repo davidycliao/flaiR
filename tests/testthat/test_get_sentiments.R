7d77c564ba9ba9c22d3ff0027d3882b82a98a6b5
David Liao <35374714+davidycliao@users.noreply.github.com>
2023-09-02 08:41:35 -0700

    Update test_get_sentiments.R

--- tests/testthat/test_get_sentiments.R
+++ tests/testthat/test_get_sentiments.R
@@ -1,8 +1,8 @@
 test_that("get_sentiments throws an error for unsupported languages", {
-  expect_error(get_sentiments(
+  expect_equal(nrow(get_sentiments(
     texts = c("UCD is one of the best university in Ireland. ",
               "TCD in less better than Oxford"),
     doc_ids= c("doc1", "doc2"),
-    language  = "chinese"),
-    "Unsupported language. Supported languages are: sentiment, sentiment-fast, de-offensive-language.")
+    language  = "sentiment")),
+    2)
 })
