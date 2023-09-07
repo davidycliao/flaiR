58b047e5b8637e946a8310539718a69598b054d1
David Liao <35374714+davidycliao@users.noreply.github.com>
2023-09-07 04:25:57 +0100

    FIX

--- R/zzz.R
+++ R/zzz.R
@@ -10,37 +10,29 @@
   # header_footer <- "## ============================================================== ##"
   # message(header_footer)
 
-  packageStartupMessage(sprintf(" \033[1;91mflaiR\033[22;39m: An R Wrapper for Accessing Flair NLP Tagging Features %-5s", ""))
+  packageStartupMessage(sprintf(" flai\033[34mR\033[39m: An R Wrapper for Accessing Flair NLP Tagging Features %-5s", ""))
 
   # Check and report Python is installed
   if (check_python_installed()) {
-    flair_version <- get_flair_version()
     packageStartupMessage(sprintf(" Python : %-47s", reticulate::py_config()$version))
   } else {
     packageStartupMessage(sprintf(" Python : %-50s", paste0("\033[31m", "\u2717", "\033[39m")))
   }
 
   # Check and report  flair is installed
-<<<<<<< Updated upstream
-  if (check_flair_installed()) {
-    flair_version <- get_flair_version()
-    packageStartupMessage(sprintf(" Flair: %-47s", flair_version))
-=======
   if (reticulate::py_module_available("flair")) {
     packageStartupMessage(sprintf(" Flair: %-47s",  get_flair_version()))
->>>>>>> Stashed changes
   } else {
     packageStartupMessage(sprintf(" Flair: %-50s", paste0("\033[31m", "\u2717", "\033[39m")))
   }
 
-  # Check for Available Python Environment
-  if (reticulate::py_available(initialize = TRUE)) {
-    packageStartupMessage(sprintf(" py_config: %-47s", reticulate::py_config()$python))
-  } else {
-    packageStartupMessage(sprintf(" py_config: %-46s", paste0("\033[31m", "\u2717", "\033[39m")))
-  }
+  # # Check for Available Python Environment
+  # if (reticulate::py_available(initialize = TRUE)) {
+  #   packageStartupMessage(sprintf(" py_config: %-47s", reticulate::py_config()$python))
+  # } else {
+  #   packageStartupMessage(sprintf(" py_config: %-46s", paste0("\033[31m", "\u2717", "\033[39m")))
+  # }
   # message(header_footer)
 }
-
 
 
