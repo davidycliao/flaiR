613fbf4480259c256b8770a918f26327883beda3
David Liao <35374714+davidycliao@users.noreply.github.com>
2023-09-07 04:56:20 +0100

    UPDATE

--- R/zzz.R
+++ R/zzz.R
@@ -19,12 +19,15 @@
     packageStartupMessage(sprintf(" Python : %-50s", paste0("\033[31m", "\u2717", "\033[39m")))
   }
 
-  # # Check and report  flair is installed
-  # if (reticulate::py_module_available("flair")) {
-  #   packageStartupMessage(sprintf(" Flair: %-47s",  get_flair_version()))
-  # } else {
-  #   packageStartupMessage(sprintf(" Flair: %-50s", paste0("\033[31m", "\u2717", "\033[39m")))
-  # }
+  # Check and report  flair is installed
+  if (reticulate::py_module_available("flair")) {
+    packageStartupMessage(sprintf(" Flair: %-47s",  get_flair_version()))
+  } else {
+    packageStartupMessage(sprintf(" Flair: %-50s", paste0("\033[31m", "\u2717", "\033[39m")))
+    packageStartupMessage(sprintf(" Flair %-50s", paste0("is installing from Python")))
+    system(paste(reticulate::py_config()$python, "-m pip install flair"))
+
+  }
 
   # # Check for Available Python Environment
   # if (reticulate::py_available(initialize = TRUE)) {
